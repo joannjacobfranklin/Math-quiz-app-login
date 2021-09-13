9426bc5eff09af95252556ca5df7f9a689556d3f
joannjacobfranklin <78270607+joannjacobfranklin@users.noreply.github.com>
2021-09-13 15:51:59 +0530

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -1,3 +1,22 @@
+player_1 = localStorage.getItem("player 1");
+player_2 = localStorage.getItem("player 2");
+console.log(player_1 + "and" + player_2)
+
+player_1_score = 0;
+player_2_score = 0;
+
+question_turn = player_2;
+answer_turn = player_1;
+
+document.getElementById("player_1_name").innerHTML = player_1 + " - " + "&nbsp;";
+document.getElementById("player_2_name").innerHTML = player_2 + " - " + "&nbsp;";
+
+document.getElementById("player_1_score").innerHTML = player_1_score;
+document.getElementById("player_2_score").innerHTML = player_2_score;
+
+document.getElementById("question_turn_name").innerHTML = question_turn;
+document.getElementById("Answer_turn_name").innerHTML = answer_turn;
+
 function send(){
     number1 = document.getElementById("number1").value;
     number2 = document.getElementById("number2").value;
@@ -12,4 +31,55 @@ function send(){
     document.getElementById("number1").innerHTML = "";
     document.getElementById("number2").innerHTML = "";
 }
+
+function check(){
+
+    number = document.getElementById("answerInput").value;
+    console.log("The player's answer" + number);
+
+    if (number ==  Actual_answer){
+
+       if (answer_turn == player_1){
+
+           player_1_score = player_1_score + 1;
+           document.getElementById("player_1_score").innerHTML = player_1_score;
+
+       }
+       else{
+
+          player_2_score = player_2_score + 1;
+          document.getElementById("player_2_score").innerHTML = player_2_score;   
+
+       }
+
+    }
+
+    if (question_turn == player_2){
+
+        question_turn = player_1;
+        document.getElementById("question_turn_name").innerHTML = question_turn;
+        
+    }
+    else{
+
+        question_turn = player_2;
+        document.getElementById("question_turn_name").innerHTML = question_turn;
+
+    }
+
+    if (answer_turn == player_1){
+
+        answer_turn = player_2;
+        document.getElementById("Answer_turn_name").innerHTML = answer_turn;
+
+    }
+    else{
+
+        answer_turn = player_2;
+        document.getElementById("Answer_turn_name").innerHTML = answer_turn;
+
+    }
+    document.getElementById("output").innerHTML = "";
+}
+
 
